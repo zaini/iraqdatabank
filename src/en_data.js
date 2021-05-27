8710fcf2dc1e7adb06cc4df9f1c789d7172cc6ef
Ali <51179189+zaini@users.noreply.github.com>
2021-05-27 16:26:10 +0100

    Update en_data.js

--- src/en_data.js
+++ src/en_data.js
@@ -20,28 +20,32 @@ const data = [
         files: [
           {
             title: "Law of Provinces Not Associated to Regions (2008)",
-            en_source: "http://gjpi.org/wp-content/uploads/2009/01/cor_law_governorates_not_part_regions_en.pdf/",
-            ar_source: "https://arb.parliament.iq/archive/2008/11/03/%D9%82%D8%A7%D9%86%D9%88%D9%86-%D8%A7%D9%84%D9%85%D8%AD%D8%A7%D9%81%D8%B8%D8%A7%D8%AA-%D8%BA%D9%8A%D8%B1-%D8%A7%D9%84%D9%85%D9%86%D8%AA%D8%B8%D9%85%D8%A9-%D9%81%D9%8A-%D8%A7%D9%82%D9%84%D9%8A%D9%85/",
+            en_source:
+              "http://gjpi.org/wp-content/uploads/2009/01/cor_law_governorates_not_part_regions_en.pdf/",
+            ar_source:
+              "https://arb.parliament.iq/archive/2008/11/03/%D9%82%D8%A7%D9%86%D9%88%D9%86-%D8%A7%D9%84%D9%85%D8%AD%D8%A7%D9%81%D8%B8%D8%A7%D8%AA-%D8%BA%D9%8A%D8%B1-%D8%A7%D9%84%D9%85%D9%86%D8%AA%D8%B8%D9%85%D8%A9-%D9%81%D9%8A-%D8%A7%D9%82%D9%84%D9%8A%D9%85/",
           },
           {
-         topic: "Archives",  
-         files: [
-           {
-            title: "Strategic Priorities for Ministerial Plans 2014-2018",
-            en_source: "http://google.com/",
-            ar_source: "http://google.fr/",
+            topic: "Archives",
+            files: [
+              {
+                title: "Strategic Priorities for Ministerial Plans 2014-2018",
+                en_source: "http://google.com/",
+                ar_source: "http://google.fr/",
+              },
+              {
+                title: "Government Agenda 2014-2018",
+                ar_source: "http://pmo.iq/pdf/3.pdf",
+              },
+              {
+                title: "Political Agreement between Political Blocs 2014",
+                ar_source: "http://pmo.iq/pdf/4.pdf",
+              },
+            ],
           },
-           {
-        title: "Government Agenda 2014-2018",
-        ar_source: "http://pmo.iq/pdf/3.pdf",
-          },
-          {
-            title: "Political Agreement between Political Blocs 2014",
-            ar_source: "http://pmo.iq/pdf/4.pdf:,
-          }, 
         ],
       },
-   ],
- },      
+    ],
+  },
 ];
 export default data;
