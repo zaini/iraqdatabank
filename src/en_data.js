4803cca5eb54ca7882a2e1823fe8b9a5d34bce32
alialmawlawi <84853509+alialmawlawi@users.noreply.github.com>
2021-05-27 16:04:01 +0100

    Update en_data.js

--- src/en_data.js
+++ src/en_data.js
@@ -3,25 +3,31 @@ const data = [
     topic: "Executive Branch",
     files: [
       {
-        title: "Official Gazette",
-        ar_source: "http://google.fr/",
-        description: "Gazette stuff description goes here",
+        title: "Council of Ministers resolutions",
+        ar_source: "http://www.cabinet.iq/ArticlesBrowse.aspx?CatID=6/",
       },
       {
-        title: "Council of Representatives",
-        en_source: "http://google.com/",
-        ar_source: "http://google.fr/",
+        title: "Prime Minister’s Office statements",
+        en_source: "http://pmo.iq/pme/ipresse.htm/",
+        ar_source: "https://pmo.iq/press.htm/",
       },
       {
-        topic: "Council of Ministers",
+        title: "Government Agenda 2020",
+        ar_source: "https://pmo.iq/press2020/6-5-202002.htm/",
+      },
+      {
+        topic: "Local government",
         files: [
           {
-            title: "Council of Representatives",
-            en_source: "http://google.com/",
-            ar_source: "http://google.fr/",
+            title: "Law of Provinces Not Associated to Regions (2008)",
+            en_source: "http://gjpi.org/wp-content/uploads/2009/01/cor_law_governorates_not_part_regions_en.pdf/",
+            ar_source: "https://arb.parliament.iq/archive/2008/11/03/%D9%82%D8%A7%D9%86%D9%88%D9%86-%D8%A7%D9%84%D9%85%D8%AD%D8%A7%D9%81%D8%B8%D8%A7%D8%AA-%D8%BA%D9%8A%D8%B1-%D8%A7%D9%84%D9%85%D9%86%D8%AA%D8%B8%D9%85%D8%A9-%D9%81%D9%8A-%D8%A7%D9%82%D9%84%D9%8A%D9%85/",
           },
           {
-            title: "Council of Representatives",
+         topic: "Archives",  
+         files: [
+           {
+            title: "Strategic Priorities for Ministerial Plans 2014-2018",
             en_source: "http://google.com/",
             ar_source: "http://google.fr/",
           },
