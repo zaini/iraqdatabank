86ee8dbcf57842af0302b39f8d85c617a4060cf9
alialmawlawi <84853509+alialmawlawi@users.noreply.github.com>
2021-05-27 15:47:42 +0100

    Update en_data.js

--- src/en_data.js
+++ src/en_data.js
@@ -4,13 +4,13 @@ const data = [
     files: [
       {
         "title":"Council of Representatives",
-            "en_source":"http://https://en.parliament.iq/",
+            "en_source":"http://en.parliament.iq/",
             "ar_source":"http://parliament.iq/",
             "description":"Council of Representatives official website",
      files: [
       {
         "title":"Approved legislation",
-            "ar_source":"https://ar.parliament.iq/%d8%a7%d9%84%d9%82%d9%88%d8%a7%d9%86%d9%8a%d9%86-%d8%a7%d9%84%d8%b5%d8%a7%d8%af%d8%b1%d8%a9/",
+            "ar_source":"http://ar.parliament.iq/%d8%a7%d9%84%d9%82%d9%88%d8%a7%d9%86%d9%8a%d9%86-%d8%a7%d9%84%d8%b5%d8%a7%d8%af%d8%b1%d8%a9/",
       },
       {
         title: "Members of Parliament",
