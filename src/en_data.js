5c64afa17f03929bc4853b74224bfe4dabb7ef23
alialmawlawi <84853509+alialmawlawi@users.noreply.github.com>
2021-05-27 15:43:03 +0100

    Update en_data.js

--- src/en_data.js
+++ src/en_data.js
@@ -3,15 +3,18 @@ const data = [
     topic: "Legislative Branch",
     files: [
       {
-        title: "Council of Representatives",
-        en_source: "http://1001iraqithoughts.com",
-        ar_source: "http://google.fr/",
-        description: "The current council of representatives of Iraq, 2014",
+        "title":"Council of Representatives",
+            "en_source":"http://https://en.parliament.iq/",
+            "ar_source":"http://parliament.iq/",
+            "description":"Council of Representatives official website",
+     files: [
+      {
+        "title":"Approved legislation",
+            "ar_source":"https://ar.parliament.iq/%d8%a7%d9%84%d9%82%d9%88%d8%a7%d9%86%d9%8a%d9%86-%d8%a7%d9%84%d8%b5%d8%a7%d8%af%d8%b1%d8%a9/",
       },
       {
-        title: "Official Gazette",
-        ar_source: "http://google.fr/",
-        description: "Gazette stuff",
+        title: "Members of Parliament",
+        ar_source: "https://ar.parliament.iq/%d8%a7%d9%84%d9%86%d9%88%d8%a7%d8%a8-2/",
       },
       {
         title: "Council of Representatives",
