028e28cb461779575a1e6227e17c92d3591d2317
alialmawlawi <84853509+alialmawlawi@users.noreply.github.com>
2021-05-27 15:52:56 +0100

    Update en_data.js

--- src/en_data.js
+++ src/en_data.js
@@ -1,33 +1,5 @@
 const data = [
   {
-    topic: "Legislative Branch",
-    files: [
-      {
-        "title":"Council of Representatives",
-            "en_source":"http://en.parliament.iq/",
-            "ar_source":"http://parliament.iq/",
-            "description":"Council of Representatives official website",
-     files: [
-      {
-        "title":"Approved legislation",
-            "ar_source":"http://ar.parliament.iq/%d8%a7%d9%84%d9%82%d9%88%d8%a7%d9%86%d9%8a%d9%86-%d8%a7%d9%84%d8%b5%d8%a7%d8%af%d8%b1%d8%a9/",
-      },
-      {
-        title: "Members of Parliament",
-        ar_source: "https://ar.parliament.iq/%d8%a7%d9%84%d9%86%d9%88%d8%a7%d8%a8-2/",
-      },
-      {
-        title: "Council of Representatives",
-        en_source: "http://google.com/",
-      },
-      {
-        title: "Council of Representatives",
-        en_source: "http://google.com/",
-        ar_source: "http://google.fr/",
-      },
-    ],
-  },
-  {
     topic: "Executive Branch",
     files: [
       {
@@ -52,46 +24,6 @@ const data = [
             title: "Council of Representatives",
             en_source: "http://google.com/",
             ar_source: "http://google.fr/",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    topic: "Judicial Branch",
-    files: [
-      {
-        title: "Council of Representatives",
-        en_source: "http://google.com/",
-        ar_source: "http://google.fr/",
-      },
-      {
-        topic: "Council of Ministers",
-        files: [
-          {
-            title: "Council of Representatives",
-            en_source: "http://google.com/",
-            ar_source: "http://google.fr/",
-          },
-          {
-            title: "Council of Representatives",
-            en_source: "http://google.com/",
-            ar_source: "http://google.fr/",
-          },
-          {
-            topic: "Council of Ministers",
-            files: [
-              {
-                title: "Council of Representatives",
-                en_source: "http://google.com/",
-                ar_source: "http://google.fr/",
-              },
-              {
-                title: "Council of Representatives",
-                en_source: "http://google.com/",
-                ar_source: "http://google.fr/",
-              },
-            ],
           },
         ],
       },
