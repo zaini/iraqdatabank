b7b6846e3f9c41175f15bd2aa9f53e795495ded5
alialmawlawi <84853509+alialmawlawi@users.noreply.github.com>
2021-05-27 21:16:05 +0100

    Update en_data.js

--- src/en_data.js
+++ src/en_data.js
@@ -70,5 +70,35 @@ const data = [
       },
    ],
  } 
+  {
+    topic: "Judicial Branch",
+    files: [
+      {
+        title: "Official Gazette",
+        ar_source: "http://google.fr/",
+        description: "Gazette stuff description goes here",
+      },
+      {
+        title: "Council of Representatives",
+        en_source: "http://google.com/",
+        ar_source: "http://google.fr/",
+      },
+      {
+        topic: "Council of Ministers",
+        files: [
+          {
+            title: "Council of Representatives",
+            en_source: "http://google.com/",
+            ar_source: "http://google.fr/",
+          },
+          {
+            title: "Council of Representatives",
+            en_source: "http://google.com/",
+            ar_source: "http://google.fr/",
+          },
+        ],
+      },
+    ],
+  },
 ];
 export default data;
