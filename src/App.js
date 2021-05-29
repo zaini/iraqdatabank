d8cc073c436e020c0f1ac23cde9aed42f20289f7
zaini <alimzaini@outlook.com>
2021-05-29 18:15:14 +0100

    Add twitter, update title, add logo

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import { Center } from "@chakra-ui/layout";
 import { Heading } from "@chakra-ui/layout";
 import DataViewer from "./DataViewer";
 import Footer from "./Footer";
+import Logo from "./logo.png";
+import { Image } from "@chakra-ui/react";
 
 const App = () => {
   return (
     <Container>
       <Center m="10px">
-        <Heading fontSize="50px">Iraq Data Bank</Heading>
+        <Heading fontSize="50px">Iraq Databank</Heading>
       </Center>
       <Center>
-        <Heading>بنك المعلومات العراقي</Heading>
+        <Image boxSize="200px" src={Logo} />
+      </Center>
+      <Center>
+        <Heading fontSize="50px">بنك المعلومات للعراق</Heading>
       </Center>
       <Divider />
       <DataViewer />
--- src/Footer.js
+++ src/Footer.js
@@ -14,13 +14,22 @@ const Footer = () => {
     >
       <Stack direction="row" spacing="4" align="center" justify="space-between">
         <Box>
-          <a href="/">English الإنجليزية</a>
+          <a href="/">English | الإنجليزية</a>
         </Box>
         <Box>
-          <a href="mailto:info@iraqdatabank.org">Contact 📧 اتصل</a>
+          <a href="mailto:info@iraqdatabank.org">Contact | اتصل</a>
         </Box>
         <Box>
-          <a href="/ar">Arabic العربية</a>
+          <a
+            href="https://twitter.com/iraqdatabank"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Twitter | تويتر
+          </a>
+        </Box>
+        <Box>
+          <a href="/ar">Arabic | العربية</a>
         </Box>
       </Stack>
 
@@ -33,7 +42,7 @@ const Footer = () => {
           rel="noreferrer"
           style={{ color: "gray", fontSize: "13px" }}
         >
-          2021 &copy; Ali Zaini علي زيني
+          2021 &copy; Ali Zaini | علي زيني
         </a>
       </Box>
     </Box>
